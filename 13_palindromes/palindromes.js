834532ea8d7754e5e19b0005cac885a558b083d6
artur-bertash <arturbertash88@gmail.com>
2025-06-04 15:29:39 -0600

    last ones in foundations

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,24 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let left = 0
+    let right = string.length - 1
+    string = string.toLowerCase()
+    const isAlphaNumeric = (char) => /[a-z0-9]/.test(char);
+    while (left < right) {
 
+        while (!isAlphaNumeric(string[left])) {
+            left++
+        }
+        while (!isAlphaNumeric(string[right])) {
+            right--
+        }
+        if (string[left] !== string[right]) {
+            return false
+        }
+        left++
+        right--
+    }
+    return true
 };
-
+console.log(palindromes("A car, a man, a maraca.'"))
 // Do not edit below this line
 module.exports = palindromes;
--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+    let oldest = 0
+    people.forEach(person => {
+        let age = person.yearOfDeath - person.yearOfBirth
+        if (age > oldest) {
+            oldest = age
+            let res = person
+        }
 
+    });
+    return person
 };
 
 // Do not edit below this line
